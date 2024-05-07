fb08312517241233fda5f24ca1b8234f69ec4837
GeekySazzad <71886049+sazzadalam112@users.noreply.github.com>
2024-05-07 18:32:33 +0530

    initial-commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Heading = () => {
+    return (
+        <h1>hello</h1>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Heading />)
+
