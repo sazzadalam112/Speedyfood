bec631687db75d221977e68f1db5741e5bfd2a60
GeekySazzad <71886049+sazzadalam112@users.noreply.github.com>
2024-05-09 23:48:09 +0530

    use-state

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,29 @@
 import { restaurantList } from "../Contants";
 import Restaurantcart from "./Restaurantcart";
 import Cuisines from "./Cuisines";
+import { useState } from "react";
 
 const Body = () => {
+
+    const [searchtext, setSearchtext] = useState("KFC");
     return (
         <>
             < Cuisines />
+            <div className="px-80">
+                <input
+                    type="text"
+                    className="bg-black text-red-500 p-3 rounded-2xl"
+                    value={searchtext}
+                    placeholder="search"
+                    onChange={(e) => {
+                        setSearchtext(e.target.value);
+                    }}
+                />
+
+                <button className="mx-10 bg-gray-700 p-3 rounded-2xl px-5">Submit</button>
+
+            </div>
+
             <div className="flex flex-wrap px-0 mx-60">
                 {
                     restaurantList.map((restaurant) => {
--- src/components/Cuisines.js
+++ src/components/Cuisines.js
@@ -3,7 +3,7 @@ const Cuisines = () => {
     return (
         <div className="px-60 ">
             <h1 className='font-bold text-4xl py-5'>what's on your Mind ?</h1>
-            <div className='flex h-60'>
+            <div className='flex  h-60'>
                 <img
 
                     src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029850/PC_Creative%20refresh/3D_bau/banners_new/Dosa.png" />
